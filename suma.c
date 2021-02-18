9d18fae19f8aa641a183c736379d1f9058f3b0d6
Francisco Sanchez Salomon <hss_francisco@hotmail.com>
2021-02-17 22:31:28 -0600

    first commit

---
+++ suma.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int x, y;
+    printf("Enter the first integer: \n");
+    scanf("%d", &x);
+    printf("Enter the second integer: \n");
+    scanf("%d", &y);
+    printf("Suma = %d\n",x + y);
+    return 0;
+}
